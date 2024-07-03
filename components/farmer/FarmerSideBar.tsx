c84d3532269b157758b84ff3b7f218f30ea5e6e7
TeddyMuli <tmuli974@gmail.com>
2024-07-03 08:50:24 +0300

    add links to sidebar

--- components/farmer/FarmerSideBar.tsx
+++ components/farmer/FarmerSideBar.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import React from 'react';
+import { farmerLinks } from '@/constants';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const FarmerSideBar = () => {
+  const pathname = usePathname();
+
   return (
-    <div>
-      FarmerSiseBar
+    <div className='bg-customDarkGrey p-4 w-[17%] flex justify-center pt-8'>
+      <div className='flex flex-col gap-4'>
+        {farmerLinks.map((link, index) => {
+          return (
+            <Link key={index} href={link.path} className={`text-white text-lg hover:bg-white hover:text-customDark active:bg-white p-3 rounded-3xl transition-all duration-200 ${link.path === pathname && "text-customDark bg-white"}`}>
+              {link.name}
+            </Link>
+          )
+        })}
+      </div>
     </div>
   );
 }
