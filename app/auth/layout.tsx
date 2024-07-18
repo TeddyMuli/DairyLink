5c1e3e3e852c4395dd7c10d86608b76b79ee754d
TeddyMuli <tmuli974@gmail.com>
2024-07-18 10:56:27 +0300

    Redirect user from auth pages to respective pages if user is logged in

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,23 @@
+import { getUser } from "@/components/supabase/GetUser";
+import { redirect } from "next/navigation";
 import { FC } from "react";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+const AuthLayout: FC<AuthLayoutProps> = async ({ children }) => {
+  const user = await getUser();
+  if (user) {
+    if (user?.user_metadata?.accountType === "Farmer") { 
+      redirect("/app");
+    } else if (user?.user_metadata?.accountType === "Cooperative") {
+      redirect("/admin");
+    } else {
+      return null;
+    }
+  }
+  
   return (
     <main className="antialiased flex min-h-screen flex-col items-center justify-center bg-slate-100">
       {children}
